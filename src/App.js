5f10e49890e7016fa511dfcd81b0193b747f6083
Chris <c.odegard@gmail.com>
2016-11-07 18:05:45 -0500

    added DocumentCard component as example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import { HorizontalBar, Heading } from './MyComponents';
 import Page1 from './PageContent';
+import {
+  DocumentCard,
+  DocumentCardPreview,
+  DocumentCardTitle,
+  DocumentCardActivity
+} from 'office-ui-fabric-react/lib/DocumentCard';
 
 class App extends Component {
   render() {
@@ -10,7 +16,28 @@ class App extends Component {
         <div className="ms-Grid">
           <Heading />
           <HorizontalBar />
-          <Page1 />
+          <DocumentCard onClickHref='http://bing.com'>
+            <DocumentCardPreview
+              previewImages={[
+                {
+                  previewImageSrc: require('./documentpreview.png'),
+                  iconSrc: require('./iconppt.png'),
+                  width: "100%",
+                  height: "100%",
+                  accentColor: '#ce4b1f'
+                }
+              ]}
+              />
+            <DocumentCardTitle title='Revenue stream proposal fiscal year 2016 version02.pptx' />
+            <DocumentCardActivity
+              activity='Created Feb 23, 2016'
+              people={
+                [
+                  { name: 'Kat Larrson', profileImageSrc: require('./avatarkat.png') }
+                ]
+              }
+              />
+          </DocumentCard>
           <HorizontalBar />
         </div>
       </div>
