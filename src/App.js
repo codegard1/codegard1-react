cd69534830eaec5ad59e384036e8bae995898c00
Chris Odegard <c.odegard@gmail.com>
2016-11-13 19:32:32 -0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-//eslint-disable-next-line
-import { HorizontalBar, Heading, Heading1 } from './MyComponents';
-import Page1 from './PageContent';
+import { HorizontalBar, Heading, Page } from './MyComponents';
 
 class App extends Component {
   constructor(props) {
@@ -10,20 +8,19 @@ class App extends Component {
     this.state = { page: 'home' }
   }
   
-  changePage(o) {
+  changePage(p) {
     this.setState({
-      page: o
+      page: p
     })
   }
 
   render() {
-    var page = this.state.page;
     return (
       <div className="App">
         <div className="ms-Grid ms-u-fadeIn400">
-          <Heading1 selectedKey={page} changePage={this.changePage.bind(this)} />
+          <Heading selectedKey={this.state.page} changePage={this.changePage.bind(this)} />
           <HorizontalBar />
-          <Page1 page={page} />
+          <Page page={this.state.page} />
           <HorizontalBar />
         </div>
       </div>
--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { Link } from 'office-ui-fabric-react/lib/Link';
+import { Experimental } from './PageContent';
 
-class Heading1 extends Component{
+class Heading extends Component{
     handleChange (options) {
         this.props.changePage(options.key);
     }
@@ -10,9 +12,8 @@ class Heading1 extends Component{
         return (
             <div className="ms-Grid-row">
                 <div className="ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg9 ms-u-xl10 header">
-                    <span className="ms-font-xxl ms-fontColor-tealDark ms-u-fadeIn400">
-                        <strong>codegard1</strong> &nbsp;
-                        <i className="ms-Icon ms-Icon--circleCoffee"></i>
+                    <span className="ms-font-xl ms-fontColor-tealDark ms-u-fadeIn400">
+                        <strong>codegard1</strong>
                     </span>
                 </div>
                 <div className="ms-Grid-col ms-u-hiddenSm ms-u-md4 ms-u-lg3 ms-u-xl2 header">
@@ -22,7 +23,8 @@ class Heading1 extends Component{
                             [
                                 { key: 'home', text: 'Home' },
                                 { key: 'identity', text: 'Identity' },
-                                { key: 'projects', text: 'Projects' }
+                                { key: 'projects', text: 'Projects' },
+                                { key: 'experimental', text: 'Experimental' }
                             ]
                         }
                         selectedKey={this.props.selectedKey}
@@ -62,8 +64,130 @@ class Topping extends Component{
     }
 };
 
+class Page extends Component {
+    render () {
+        const left = "ms-Grid-col ms-u-hiddenSm ms-u-md1 ms-u-lg1 ms-u-xl1 ms-u-xxl2";
+        const inner = "ms-u-slideRightIn40 ms-Grid-col ms-u-sm12 ms-u-md10 ms-u-lg8 ms-u-xl7 ms-u-xxl5";
+        const right = "ms-Grid-col ms-u-hiddenSm ms-u-md1 ms-u-lg3 ms-u-xl4 ms-u-xxl5";
+
+        switch (this.props.page) {
+            case 'home':
+                return (
+                    <div className="ms-Grid-row">
+                        <div className={left}>&nbsp;</div>
+
+                        <div className={inner}>
+                            <Topping title="Home" icon="home" />
+                            <div className="ms-font-xl">
+                                <p>
+                                    Hello, my name is Chris. Here are some facts about me:</p>
+                                <ul>
+                                    <li>I work at Macys Merchandising Group as a SharePoint Developer</li>
+                                    <li>I make business applications in SharePoint with a heavy emphasis on custom code and re-usable solutions</li>
+                                    <li>Some of my other interests include, but are not limited to:
+                                        <ul>
+                                            <li>SharePoint Framework</li>
+                                            <li>Nintex Workflows</li>
+                                            <li>jQuery</li>
+                                            <li>React</li>
+                                            <li>ECMAScript 6</li>
+                                            <li>NodeJS</li>
+                                            <li>Python</li>
+                                            <li>Fabric UI</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <div className={right}>&nbsp;</div>
+                    </div> /* end ms-Grid-row */
+                );
+                // eslint-disable-next-line
+                break;
+
+            case 'identity':
+                return (
+                    <div className="ms-Grid-row">
+                        <div className={left}>&nbsp;</div>
+
+                        <div className={inner}>
+                            <Topping title="Identity" icon="contact" />
+                            <div className="ms-font-xl">
+                                <p>Email: <Link href="mailto:c.odegard@gmail.com">c.odegard[at]gmail.com</Link></p>
+                                <p>
+                                    Here are some links to other online internet websites where I take residence:
+                                    <ul>
+                                        <li>I have a <Link href="https://www.linkedin.com/in/codegard1">LinkedIn profile</Link></li>
+                                        <li>I have a <Link href="https://github.com/codegard1">GitHub profile</Link></li>
+                                        <li>I use <Link href="https://jsfiddle.net/user/codegard1/">JSFiddle</Link> and sometimes <Link href="http://plnkr.co/users/codegard1">Plunker</Link>              to test ideas</li>
+                                        <li>I used to make a lot of electronic music in college, and many of those songs can be found on my <Link href="https://soundcloud.com/ciaervo">SoundCloud profile</Link></li>
+                                    </ul>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={right}>&nbsp;</div>
+                    </div> /* end ms-Grid-row */
+                );
+                // eslint-disable-next-line
+                break;
+
+            case 'projects':
+                return (
+                    <div className="ms-Grid-row">
+                        <div className={left}>&nbsp;</div>
+
+                        <div className={inner}>
+                            <Topping title="Current Projects" icon="heartEmpty" />
+                            <div className="ms-font-xl">
+                                <p>
+                                    Here is a sampling of what I am actively working on:</p>
+                                <ul>
+                                    <li>Using <Link href="http://datatables.net/">Datatables</Link> to display SharePoint List data in an on-premise Site Collection.</li>
+                                    <li>Embedding a Yammer feed in an on-premise SharePoint page, using single sign-on</li>
+                                    <li>Getting used to the <Link href="https://github.com/petehunt/react-howto">React ecosystem</Link>, with the intention
+                                of using the framework to create modular forms within SharePoint pages, obviating some of our dependence on
+                                <Link href="http://www.nintex.com/">Nintex Forms</Link></li>
+                                    <li>Expanding this site</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className={right}>&nbsp;</div>
+                    </div> /* end ms-Grid-row */
+                );
+                // eslint-disable-next-line
+                break;
+
+            case 'experimental':
+                return (
+                    <div className="ms-Grid-row">
+                        <div className={left}>&nbsp;</div>
+
+                        <div className={inner}>
+                            <Topping title="Experimental" icon="circle" />
+                            <div className="ms-font-xl">
+                                <p>Yo World</p>
+                            </div>
+                        </div>
+
+                        <div className={right}>&nbsp;</div>
+                    </div>
+                );
+            // eslint-disable-next-line
+            break;
+
+
+            default:
+                return <div>No content</div>;
+        } // end switch
+    }
+}
+
 module.exports = { 
-    Heading1, 
+    Heading, 
     HorizontalBar, 
-    Topping 
+    Topping, 
+    Page
 };
