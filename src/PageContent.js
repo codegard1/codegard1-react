1649133d36fe66dd579f040ce5a6b66bca2f9110
Chris Odegard <c.odegard@gmail.com>
2016-11-14 20:11:39 -0500

    light editing

--- src/PageContent.js
+++ src/PageContent.js
@@ -21,18 +21,18 @@ class Home extends Component {
                     <p>
                         Hello, my name is Chris. Here are some facts about me:</p>
                     <ul>
-                        <li>I work at Macys Merchandising Group as a SharePoint Developer</li>
-                        <li>I make business applications in SharePoint with a heavy emphasis on custom code and re-usable solutions</li>
+                        <li>I am a SharePoint Developer at Macy's</li>
+                        <li>I make business applications in SharePoint with a lot of custom code</li> 
+                        <li>I prefer to make re-usable solutions</li>
                         <li>Some of my other interests include, but are not limited to:
                             <ul>
+                                <li>ES6 6</li>
+                                <li>NodeJS</li>
+                                <li>Fabric UI</li>
+                                <li>React</li>
                                 <li>SharePoint Framework</li>
                                 <li>Nintex Workflows</li>
                                 <li>jQuery</li>
-                                <li>React</li>
-                                <li>ECMAScript 6</li>
-                                <li>NodeJS</li>
-                                <li>Python</li>
-                                <li>Fabric UI</li>
                             </ul>
                         </li>
                     </ul>
@@ -54,8 +54,8 @@ class Identity extends Component {
                                     <ul>
                                         <li>I have a <Link href="https://www.linkedin.com/in/codegard1">LinkedIn profile</Link></li>
                                         <li>I have a <Link href="https://github.com/codegard1">GitHub profile</Link></li>
-                                        <li>I use <Link href="https://jsfiddle.net/user/codegard1/">JSFiddle</Link> and sometimes <Link href="http://plnkr.co/users/codegard1">Plunker</Link>              to test ideas</li>
-                                        <li>I used to make a lot of electronic music in college, and many of those songs can be found on my <Link href="https://soundcloud.com/ciaervo">SoundCloud profile</Link></li>
+                                        <li>I use <Link href="https://jsfiddle.net/user/codegard1/">JSFiddle</Link> and sometimes <Link href="http://plnkr.co/users/codegard1">Plunker</Link> to test ideas</li>
+                                        <li>I used to make a lot of electronic music, and many of those songs can be found on my <Link href="https://soundcloud.com/ciaervo">SoundCloud profile</Link></li>
                                     </ul>
                                 </p>
                             </div>
@@ -73,12 +73,12 @@ class Projects extends Component {
                                 <p>
                                     Here is a sampling of what I am actively working on:</p>
                                 <ul>
-                                    <li>Using <Link href="http://datatables.net/">Datatables</Link> to display SharePoint List data in an on-premise Site Collection.</li>
+                                    <li>Using <Link href="http://datatables.net/">Datatables</Link> to display SharePoint List data</li>
                                     <li>Embedding a Yammer feed in an on-premise SharePoint page, using single sign-on</li>
                                     <li>Getting used to the <Link href="https://github.com/petehunt/react-howto">React ecosystem</Link>, with the intention
-                                of using the framework to create modular forms within SharePoint pages, obviating some of our dependence on
+                                of using the framework to create modular forms within SharePoint pages, lessening dependence on 
                                 <Link href="http://www.nintex.com/">Nintex Forms</Link></li>
-                                    <li>Expanding this site</li>
+                                    <li>Making this site more of a web application</li>
                                 </ul>
                             </div>
             </div>
