959e0fc82caa07f4b813d008c0e0ac6ad95b15af
Chris <c.odegard@gmail.com>
2016-12-01 10:09:50 -0500

    bigger logo

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -13,21 +13,19 @@ class Heading extends Component{
         return (
             <div className="ms-Grid-row">
                 <div className="ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg9 ms-u-xl10 header">
-                    <span className="ms-font-xl ms-fontColor-tealDark ms-u-fadeIn400">
+                    <span className="ms-font-su ms-fontColor-tealDark ms-u-fadeIn400">
                         <strong>codegard1</strong>
                     </span>
                 </div>
                 <div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg3 ms-u-xl2 header">
                     <Dropdown
                         label=''
-                        options={
-                            [
+                        options={[
                                 { key: 'home', text: 'Home' },
                                 { key: 'identity', text: 'Identity' },
                                 { key: 'projects', text: 'Projects' },
                                 { key: 'experimental', text: 'Experimental' }
-                            ]
-                        }
+                            ]}
                         selectedKey={this.props.selectedKey}
                         onChanged={this.handleChange.bind(this)}
                         />
