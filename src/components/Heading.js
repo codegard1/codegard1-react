2513d9de281f41e5b725ec7603b2a14fef6d93e7
Chris <c.odegard@gmail.com>
2016-12-15 16:43:07 -0500

    style update and css fix

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import * as fabric from './fabricStyles';
 
 export class Heading extends Component {
     handleChange (options) {
@@ -7,14 +8,16 @@ export class Heading extends Component {
     }
     
     render () {
+        let fontColor = fabric.fontColor;
+
         return (
             <div className="ms-Grid-row">
-                <div className="ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg9 ms-u-xl10 header">
-                    <span className="ms-font-su ms-fontColor-tealDark ms-u-fadeIn400">
+                <div className="ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg9 ms-u-xl10 ms-bgColor-neutralLighter header">
+                    <span className={"ms-font-su ms-u-fadeIn400 " + fontColor}>
                         <strong>codegard1</strong>
                     </span>
                 </div>
-                <div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg3 ms-u-xl2 header">
+                <div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg3 ms-u-xl2 ms-bgColor-neutralLighter header">
                     <Dropdown
                         label=''
                         options={[
