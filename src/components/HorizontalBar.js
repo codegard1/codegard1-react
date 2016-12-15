fc9a6e9df7f3ba286201bfba6a5ee139d356926b
Chris <c.odegard@gmail.com>
2016-12-15 16:30:31 -0500

    import color from fabricStyles

--- src/components/HorizontalBar.js
+++ src/components/HorizontalBar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import * as fabric from './fabricStyles';
 
 export class HorizontalBar extends Component {
     render () {
+        let bgColor = fabric.bgColor;
+
         return (
-            <div className="ms-Grid-row ms-bgColor-teal">
+            <div className={"ms-Grid-row " + bgColor}>
                 <div className="ms-Grid-col ms-u-sm12">&nbsp;</div>
             </div>
         );
