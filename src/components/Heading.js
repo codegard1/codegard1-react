4f1e7e72983eaea471329cbaf61c7c06d5501c85
Chris Odegard <c.odegard@gmail.com>
2017-07-20 19:23:52 -0400

    cleaned up code

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,86 +1,45 @@
-import React, { Component } from 'react';
-import * as T from 'prop-types';
-import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
-import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
-import * as fabric from './fabricStyles';
+import React, { Component } from "react";
+import * as T from "prop-types";
+import { Dropdown } from "office-ui-fabric-react/lib/Dropdown";
+import { CommandBar } from "office-ui-fabric-react/lib/CommandBar";
 
+/* custom stuff */
+import * as fabric from "./fabricStyles";
 
+export function Heading (props) {
+    const fontColor = fabric.fontColor;
 
-export class Heading extends Component {
-    handleChange(options) {
-        this.props._changePage(options.key);
-    }
+    return (
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-u-sm8 ms-u-md9 ms-u-lg12 ms-bgColor-neutralLighter header">
+          <span className={"ms-font-su ms-u-fadeIn400 " + fontColor}>
+            <strong>codegard1</strong>
+          </span>
+        </div>
 
-    render() {
-        let commandBarDefinition = {
-            items: [
-                {
-                    key: 'options',
-                    name: 'Options',
-                    ariaLabel: 'Site options menu',
-                    subMenuProps: {
-                        items: [
-                            {
-                                key: 'home',
-                                name: 'Home',
-                                ariaLabel: 'Home',
-                            },
-                            {
-                                key: 'identity',
-                                name: 'Identity',
-                                ariaLabel: 'Identity',
-                            },
-                            {
-                                key: 'experimental',
-                                name: 'Experimental',
-                                ariaLabel: 'Experimental',
-                            },
-                        ],
-                        isSubMenu: true,
-                        isBeakVisible: true,
-                    },
-                },
-            ]
-        };
-        const fontColor = fabric.fontColor;
+        <div className="ms-Grid-col ms-u-sm4 ms-u-md3 ms-u-hiddenLgUp ms-bgColor-neutralLighter header ms-u-fadeIn400">
+          <Dropdown
+            options={[
+              { key: "home", text: "Home" },
+              { key: "identity", text: "Identity" },
+              { key: "projects", text: "Projects" },
+              { key: "fabric-callout", text: "Fabric Callout" },
+              { key: "color-box", text: "Color Box" },
+              { key: "fabric-list", text: "Fabric List" },
+              { key: "basic-form", text: "Basic Form" },
+              { key: "fabric-callout", text: "Fabric Callout" }
+            ]}
+            selectedKey={props.selectedKey}
+            onChanged={options => props._changePage(options.key)}
+          />
+        </div>
+      </div>
+    );
+  }
 
-        return (
-            <div className="ms-Grid-row">
-                <div className="ms-Grid-col ms-u-sm8 ms-u-md9 ms-u-lg12 ms-bgColor-neutralLighter header">
-                    <span className={"ms-font-su ms-u-fadeIn400 " + fontColor}>
-                        <strong>codegard1</strong>
-                    </span>
-                </div>
-
-                <div className="ms-Grid-col ms-u-sm4 ms-u-md3 ms-u-hiddenLgUp ms-bgColor-neutralLighter header ms-u-fadeIn400">
-                    {/*<CommandBar
-                        isSearchBoxVisible={false}
-                        elipisisAriaLabel='Navigation'
-                        items={commandBarDefinition.items}
-                    />*/}
-                    <Dropdown
-
-                        label=''
-                        options={[
-                            { key: 'home', text: 'Home' },
-                            { key: 'identity', text: 'Identity' },
-                            { key: 'projects', text: 'Projects' },                            
-                            { key: 'fabric-callout',text:"Fabric Callout" },
-                            { key: 'color-box', text: 'Color Box' },
-                            { key: 'fabric-list', text: 'Fabric List' },
-                            { key: 'basic-form', text: 'Basic Form' },
-                            { key: 'fabric-callout', text: 'Fabric Callout'},
-                        ]}
-                        selectedKey={this.props.selectedKey}
-                        onChanged={this.handleChange.bind(this)} />
-                </div>
-            </div>
-        );
-    }
+Heading.propTypes = {
+  selectedKey: T.string.isRequired,
+  _changePage: T.func.isRequired
 };
 
-/* ES6 Syntax */
-Heading.propTypes = {
-    selectedKey: T.string.isRequired,
-    _changePage: T.func.isRequired
-}
+export default Heading;
