7a22212eda5b3d7dd36d4c584ee977364c9ffdbe
Chris Odegard <c.odegard@gmail.com>
2017-07-20 19:32:41 -0400

    cleaned up Topping

--- src/components/Topping.js
+++ src/components/Topping.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import * as T from 'prop-types';
+import React from "react";
+import * as T from "prop-types";
 
-import { fabricColor } from './fabricStyles';
+/* custom stuff */
+import { fabricColor } from "./fabricStyles";
 
-export class Topping extends Component {
-    render() {
-        let icon = this.props.icon;
-        let title = this.props.title;
-        // TODO: pass in bgColor and fontColor as props
-        let bgColor = fabricColor('bg', 'tealLight');
-        let fontColor = fabricColor('font', 'tealDark');
+export function Topping(props) {
+  /**
+   * @todo pass in bgColor and fontColor as props
+   */
+  const bgColor = fabricColor("bg", "tealLight");
+  const fontColor = fabricColor("font", "tealDark");
 
-        return (
-            <div className={"ms-Grid-col ms-u-sm12 topping " + bgColor}>
-                <span className={"ms-font-xxl " + fontColor}>
-                    {title} &nbsp;
-                        <i className={"ms-Icon ms-Icon--" + icon}></i>
-                </span>
-            </div>
-        );
-    }
-};
+  return (
+    <div className={"ms-Grid-col ms-u-sm12 topping " + bgColor}>
+      <span className={"ms-font-xxl " + fontColor}>
+        {props.title} &nbsp;
+        <i className={"ms-Icon ms-Icon--" + props.icon} />
+      </span>
+    </div>
+  );
+}
 
 Topping.propTypes = {
-    icon: T.string,
-    title: T.string.isRequired
-}
+  icon: T.string.isRequired,
+  title: T.string.isRequired
+};
+
+export default Topping;
