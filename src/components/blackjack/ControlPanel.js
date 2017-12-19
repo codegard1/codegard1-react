08aa89d06fec88c4100a08cf0dc440a1349ae69d
Chris Odegard <c.odegard@gmail.com>
2017-12-18 20:11:40 -0500

    commented out extraneous lines

--- src/components/blackjack/ControlPanel.js
+++ src/components/blackjack/ControlPanel.js
@@ -11,72 +11,73 @@ class ControlPanel extends Component {
   render() {
     /* selectedFlag is true when the player has selected cards in his hand */
     let selectedFlag = this.props.selectedFlag;
-    let selectedCards = selectedFlag
-      ? DeckStore.getSelected(this.props.playerId)
-      : [];
+    /* Flag used by put / draw menu items */
+    // let selectedCards = selectedFlag
+    //   ? DeckStore.getSelected(this.props.playerId)
+    //   : [];
     /* when gameStatusFlag is TRUE, most members of blackJackItems are disabled */
     const gameStatusFlag = this.props.gameStatusFlag;
     const playerStatusFlag = this.props.playerStatusFlag;
     const npcFlag = this.props.player.isNPC;
 
-    const drawItems = [
-      {
-        key: "draw",
-        name: "Draw",
-        ariaLabel: "Draw",
-        iconProps: "",
-        disabled: false,
-        onClick: AppActions.draw
-      },
-      {
-        key: "draw-from-bottom-of-deck",
-        name: "Draw from Bottom of Deck",
-        ariaLabel: "Draw from Bottom of Deck",
-        iconProps: "",
-        disabled: false,
-        onClick: AppActions.drawFromBottomOfDeck
-      },
-      {
-        key: "draw-random",
-        name: "Draw Random",
-        ariaLabel: "Draw Random",
-        iconProps: "",
-        disabled: false,
-        onClick: AppActions.drawRandom
-      }
-    ];
-    const putItems = [
-      {
-        key: "put-on-top-of-deck",
-        name: "Put on Top of Deck",
-        ariaLabel: "Put on Top of Deck",
-        iconProps: "",
-        disabled: false,
-        onClick: ev => {
-          ev.preventDefault();
-          AppActions.putOnTopOfDeck(this.props.playerId, selectedCards);
-          AppActions.removeSelectedFromPlayerHand(selectedCards);
-        }
-      },
-      {
-        key: "put-on-bottom-of-deck",
-        name: "Put on Bottom of Deck",
-        ariaLabel: "Put on Bottom of Deck",
-        iconProps: "",
-        disabled: false,
-        onClick: cards => {
-          AppActions.putOnBottomOfDeck(cards);
-          AppActions.removeSelectedFromPlayerHand(cards);
-        }
-      }
-    ];
+    // const drawItems = [
+    //   {
+    //     key: "draw",
+    //     name: "Draw",
+    //     ariaLabel: "Draw",
+    //     iconProps: "",
+    //     disabled: false,
+    //     onClick: AppActions.draw
+    //   },
+    //   {
+    //     key: "draw-from-bottom-of-deck",
+    //     name: "Draw from Bottom of Deck",
+    //     ariaLabel: "Draw from Bottom of Deck",
+    //     iconProps: "",
+    //     disabled: false,
+    //     onClick: AppActions.drawFromBottomOfDeck
+    //   },
+    //   {
+    //     key: "draw-random",
+    //     name: "Draw Random",
+    //     ariaLabel: "Draw Random",
+    //     iconProps: "",
+    //     disabled: false,
+    //     onClick: AppActions.drawRandom
+    //   }
+    // ];
+    // const putItems = [
+    //   {
+    //     key: "put-on-top-of-deck",
+    //     name: "Put on Top of Deck",
+    //     ariaLabel: "Put on Top of Deck",
+    //     iconProps: "",
+    //     disabled: false,
+    //     onClick: ev => {
+    //       ev.preventDefault();
+    //       AppActions.putOnTopOfDeck(this.props.playerId, selectedCards);
+    //       AppActions.removeSelectedFromPlayerHand(selectedCards);
+    //     }
+    //   },
+    //   {
+    //     key: "put-on-bottom-of-deck",
+    //     name: "Put on Bottom of Deck",
+    //     ariaLabel: "Put on Bottom of Deck",
+    //     iconProps: "",
+    //     disabled: false,
+    //     onClick: cards => {
+    //       AppActions.putOnBottomOfDeck(cards);
+    //       AppActions.removeSelectedFromPlayerHand(cards);
+    //     }
+    //   }
+    // ];
     let blackJackItems = [
       {
         key: "hit",
         name: "Hit",
         ariaLabel: "Hit",
         iconProps: { iconName: "Add" },
-        disabled: (gameStatusFlag || playerStatusFlag),
+        disabled: gameStatusFlag || playerStatusFlag,
         onClick: ev => {
           ev.preventDefault();
           this.props.showDeckCallout();
@@ -99,7 +100,7 @@ class ControlPanel extends Component {
         name: "Stay",
         ariaLabel: "Stay",
         iconProps: { iconName: "Forward" },
-        disabled: (gameStatusFlag || playerStatusFlag),
+        disabled: gameStatusFlag || playerStatusFlag,
         onClick: AppActions.stay
       }
     ];
@@ -119,62 +120,68 @@ class ControlPanel extends Component {
       });
     }
 
-    const drawMenu = [
-      {
-        key: "deck-menu",
-        name: "Draw",
-        ariaLabel: "Draw",
-        iconProps: "",
-        onClick(ev) {
-          ev.preventDefault();
-        },
-        subMenuProps: {
-          items: drawItems,
-          isSubMenu: true,
-          isBeakVisible: true
-        }
-      }
-    ];
-    const putMenu = [
-      {
-        key: "put-menu",
-        name: "Put",
-        ariaLabel: "Put",
-        iconProps: "",
-        onClick(ev) {
-          ev.preventDefault();
-        },
-        subMenuProps: {
-          items: putItems,
-          isSubMenu: true,
-          isBeakVisible: true
-        }
-      }
-    ];
+    /* menu items for drawing cards */
+    // const drawMenu = [
+    //   {
+    //     key: "deck-menu",
+    //     name: "Draw",
+    //     ariaLabel: "Draw",
+    //     iconProps: "",
+    //     onClick(ev) {
+    //       ev.preventDefault();
+    //     },
+    //     subMenuProps: {
+    //       items: drawItems,
+    //       isSubMenu: true,
+    //       isBeakVisible: true
+    //     }
+    //   }
+    // ];
 
-    const farItems = npcFlag ? [] : [
-      {
-        key: "options",
-        name: "",
-        ariaLabel: "Options",
-        iconProps: { iconName: "Settings" },
-        disabled: npcFlag,
-        onClick: AppActions.showOptionsPanel
-      },
-    ];
+    /* menu items for putting cards */
+    // const putMenu = [
+    //   {
+    //     key: "put-menu",
+    //     name: "Put",
+    //     ariaLabel: "Put",
+    //     iconProps: "",
+    //     onClick(ev) {
+    //       ev.preventDefault();
+    //     },
+    //     subMenuProps: {
+    //       items: putItems,
+    //       isSubMenu: true,
+    //       isBeakVisible: true
+    //     }
+    //   }
+    // ];
+
+    const farItems = npcFlag
+      ? []
+      : [
+          {
+            key: "options",
+            name: "",
+            ariaLabel: "Options",
+            iconProps: { iconName: "Settings" },
+            disabled: npcFlag,
+            onClick: AppActions.showOptionsPanel
+          }
+        ];
     // const overFlowItems = selectedFlag ? [].concat(putMenu, drawMenu) : [];
     const overFlowItems = [];
 
     return (
-      <div className="player-controlpanel" >
-        {!this.props.hidden &&
+      <div className="player-controlpanel">
+        {!this.props.hidden && (
           <CommandBar
             isSearchBoxVisible={false}
             items={blackJackItems}
             farItems={farItems}
             overflowItems={overFlowItems}
-          />}
-      </div >
+          />
+        )}
+      </div>
     );
   }
 }
