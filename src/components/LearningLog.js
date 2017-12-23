02f94cc185437fc0a2e21a89562d10c262f69dc1
Chris Odegard <c.odegard@gmail.com>
2017-12-23 17:10:16 -0500

    added sqlite3 logic but it does not compile

--- src/components/LearningLog.js
+++ src/components/LearningLog.js
@@ -1,15 +1,15 @@
 import React from "react";
-import {
-  FocusZone,
-  FocusZoneDirection
-} from "office-ui-fabric-react/lib/FocusZone";
-import { TextField } from "office-ui-fabric-react/lib/TextField";
-import { List } from "office-ui-fabric-react/lib/List";
+import {FocusZone, FocusZoneDirection} from "office-ui-fabric-react/lib/FocusZone";
+import {TextField} from "office-ui-fabric-react/lib/TextField";
+import {List} from "office-ui-fabric-react/lib/List";
 
 /* custom stuff */
-import  BaseComponent  from "./BaseComponent";
-import { learningLog2016 } from "./learningLog2016";
+import BaseComponent from "./BaseComponent";
+import {learningLog2016} from "./learningLog2016";
 import "./learninglog.css";
+
+/* todo: this should be an import */
+const sqlite3 = require('sqlite3').verbose();
 
 export class LearningLog extends BaseComponent {
   constructor(props) {
@@ -22,10 +22,55 @@ export class LearningLog extends BaseComponent {
 
     /* bind private methods */
     this._bind("_onFilterChanged", "_onRenderCell");
+  }
+
+  componentWillMount() {
+    /* array to be passed to Fabric */
+    let itemsForState = [];
+
+    /* connect to the database */
+    var db = new sqlite3.Database(":memory:");
+
+    /* create the db */
+    db.run("CREATE TABLE learninglog (key INTEGER, date TEXT, work BLOB, notes BLOB)", [], function (err) {
+      console.log(err || 'database created');
+    });
+
+    /* prepare the statement */
+    var stmt = db.prepare("insert into learninglog (key,date,work) values (?,?,?)");
+
+    /* Run the statement once for each item in the array */
+    learningLog2016.forEach(function (item, index) {
+      stmt.run(index, item.date, item.work.toString());
+    });
+
+    /* display the results */
+    db.each("select * from learninglog", [], function (err, row) {
+      console.log(err || [row.key, row.date, row.work]);
+    });
+
+    /* add the results to the output array */
+    db.each("select * from learninglog", [], function (err, row) {
+      if (!err) {
+        itemsForState.push({
+          key: row.key,
+          date: row.date,
+          work: [row.work],
+        });
+      } else {
+        console.log(err);
+      }
+    });
+
+    /* finalize the statement */
+    stmt.finalize();
+
+    /* destroy the database */
+    db.close();
 
     /* Prepare the learningLog2016 array for passing to FabricList */
-    this.itemsArray = learningLog2016.map(item => {
-      let key = item.key || Math.round(Math.random() * 10000);
+    const itemsArray = itemsForState.map(item => {
+      let key = item.key;
       let date = item.date || "no date";
       let work = item.work || [];
       let notes = item.notes || [];
@@ -43,91 +88,86 @@ export class LearningLog extends BaseComponent {
         ));
       }
 
-      return {
-        key: `learningLog-${key}`,
-        date,
-        work: workReact,
-        notes: notesReact
-      };
+      return {key: `learningLog-${key}`, date, work: workReact, notes: notesReact};
     });
-  }
+    
+    this.setState({ items: itemsArray });
+}
 
-  componentWillMount() {
-    this.setState({ items: this.itemsArray });
-  }
+// called when the filter text changes
+_onFilterChanged(text) {
+  const items = text
+    ? this
+      .state
+      .items
+      .filter(item => item.date.toLowerCase().indexOf(text.toLowerCase()) >= 0)
+    : this.state.items;
 
-  // called when the filter text changes
-  _onFilterChanged(text) {
-    const items = text
-      ? this.state.items.filter(
-          item => item.date.toLowerCase().indexOf(text.toLowerCase()) >= 0
-        )
-      : this.state.items;
+  this.setState({filterText: text, items});
+}
 
-    this.setState({
-      filterText: text,
-      items
-    });
-  }
+_onRenderCell(item, index) {
+  return (
+    <div className="itemCell" data-is-focusable={true}>
 
-  _onRenderCell(item, index) {
-    return (
-      <div className="itemCell" data-is-focusable={true}>
+      <div className="itemContent">
+        <div>
+          <span className="itemName ms-font-xl">{item.date}</span>
+          <span className="itemIndex ms-font-xs">{`#${index}`}</span>
+        </div>
+        <div className="itemDesc ms-font-s">
 
-        <div className="itemContent">
-          <div>
-            <span className="itemName ms-font-xl">{item.date}</span>
-            <span className="itemIndex ms-font-xs">{`#${index}`}</span>
-          </div>
-          <div className="itemDesc ms-font-s">
+          <p>
+            <i className="ms-Icon ms-Icon--QuickNote"/>
+            &nbsp; Work
+          </p>
 
-            <p>
-              <i className="ms-Icon ms-Icon--QuickNote" /> &nbsp; Work
-            </p>
+          <ul>
+            {item.work.length > 0
+              ? item.work
+              : "None"}
+          </ul>
 
-            <ul>
-              {item.work.length > 0 ? item.work : "None"}
-            </ul>
+          <p className="ms-font-s">
+            <i className="ms-Icon ms-Icon--QuickNote"/>
+            &nbsp; Notes
+          </p>
 
-            <p className="ms-font-s">
-              <i className="ms-Icon ms-Icon--QuickNote" /> &nbsp; Notes
-            </p>
-
-            <ul>
-              {item.notes.length > 0 ? item.notes : "None"}
-            </ul>
-          </div>
+          <ul>
+            {item.notes.length > 0
+              ? item.notes
+              : "None"}
+          </ul>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  render() {
-    let originalItems = this.itemsArray;
-    let items = this.state.items;
-    let resultCountText = items.length === originalItems.length
-      ? ""
-      : ` (${items.length} of ${originalItems.length} shown)`;
+render() {
+  let originalItems = this.itemsArray;
+  let items = this.state.items;
+  let resultCountText = items.length === originalItems.length
+    ? ""
+    : ` (${items.length} of ${originalItems.length} shown)`;
 
-    return (
-      <div id="learning-log">
-        <p className="ms-font-su">Learning Log</p>
-        <FocusZone direction={FocusZoneDirection.vertical}>
-          <TextField
-            label={`Filter by Date` + resultCountText}
-            onBeforeChange={this._onFilterChanged}
-          />
-          <List
-            items={items}
-            onRenderCell={this._onRenderCell}
-            startIndex={0}
-            renderedWindowsAhead={1}
-            renderedWindowsBehind={1}
-          />
-        </FocusZone>
-      </div>
-    );
-  }
+  return (
+    <div id="learning-log">
+      <p className="ms-font-su">Learning Log</p>
+      <FocusZone direction={FocusZoneDirection.vertical}>
+        <TextField
+          label={`Filter by Date` + resultCountText}
+          onBeforeChange={this._onFilterChanged}/>
+        <List
+          items={items}
+          onRenderCell={this._onRenderCell}
+          startIndex={0}
+          renderedWindowsAhead={1}
+          renderedWindowsBehind={1}/>
+      </FocusZone>
+    </div>
+  );
+}
 }
 
 export default LearningLog;
--- src/components/learningLog2016.js
+++ src/components/learningLog2016.js
@@ -1,246 +1,197 @@
-export const learningLog2016 = [{
+export const learningLog2016 = [
+    {
         key: 0,
         date: '12/25/2016',
         work: ['Learned about ES6 Arrow functions and variable declaration keywords '],
         notes: ['See Rapid E6 Training']
-    },
-    {
+    }, {
         key: 1,
         date: '12/22/2016',
         work: ['Learned about TypeScript on Pluralsight'],
         notes: ['See TypeScript Pluralsight Notes']
-    },
-    {
+    }, {
         key: 2,
         date: '12/14/2016',
         work: ['Watched a webinar at work about SharePoint framework'],
         notes: ['See Converting your CEWP Customizations to the SharePoint Framework']
-    },
-    {
+    }, {
         key: 3,
         date: '12/13/2016',
         work: ['Read about modules in different versions of JS'],
         notes: ['See Modules in ECMAScript']
-    },
-    {
+    }, {
         key: 4,
         date: '12/12/2016',
-        work: ['Continued the Learning React II Course on Codecademy',
-            'Learned about functional stateless components',
-            'Learned about PropTypes'
+        work: [
+            'Continued the Learning React II Course on Codecademy', 'Learned about functional stateless components', 'Learned about PropTypes'
         ],
-        notes: ['See Learn ReactJS II Notes p3',
-            'See Should I use React.createClass, ES6 Classes or stateless functional components?'
+        notes: [
+            'See Learn ReactJS II Notes p3', 'See Should I use React.createClass, ES6 Classes or stateless functional componen' +
+                    'ts?'
         ]
-    },
-    {
+    }, {
         key: 5,
         date: '12/11/2016',
-        work: ['Spent some time optimizing codegard1-react',
-            'I experimented with import/export and using index.js to make nested components more accessible'
+        work: [
+            'Spent some time optimizing codegard1-react', 'I experimented with import/export and using index.js to make nested components m' +
+                    'ore accessible'
         ],
         notes: ['See Commit log ']
-    },
-    {
+    }, {
         key: 6,
         date: '12/8/2016',
-        work: ['Continued the Learning React II Course on Codecademy',
-            'Learned about using styles in React components'
+        work: [
+            'Continued the Learning React II Course on Codecademy', 'Learned about using styles in React components'
         ],
         notes: ['See Learn React II Notes']
-    },
-    {
+    }, {
         key: 7,
         date: '12/6/2016',
-        work: ['Started Learning React II on Codecademy',
-            'Learned about Setting state from Parent, Child, and Sibling components'
+        work: [
+            'Started Learning React II on Codecademy', 'Learned about Setting state from Parent, Child, and Sibling components'
         ],
         notes: ['See Learn React II Notes']
-    },
-    {
+    }, {
         key: 8,
         date: '12/5/2016',
         work: ['Finished the react course on Codecademy '],
         notes: ['See Learn ReactJS: Part 2 Notes (Codecademy)']
-    },
-    {
+    }, {
         key: 9,
         date: '12/1/2016',
         work: ['Learned a bit more about Netlify']
-    },
-    {
+    }, {
         key: 10,
         date: '11/29/2016',
         work: [
-            'Continued learning about how to use Jade',
-            'Learned about Handlebars'
+            'Continued learning about how to use Jade', 'Learned about Handlebars'
         ],
         notes: ['See Templating Engines']
-    },
-    {
+    }, {
         key: 11,
         date: '11/28/2016',
         work: ['Learned a bit about the jade (AKA Pug) templating engine from Pluralsight'],
         notes: ['See Templating Engines']
-    },
-    {
+    }, {
         key: 12,
         date: '11/22/2016',
         work: ['Learned about jshint '],
         notes: ['See jshint Info']
-    },
-    {
+    }, {
         key: 13,
         date: '11/21/2016',
-        work: ['Started NPM course on web development using Node.js',
-            'Learned about gulp'
+        work: [
+            'Started NPM course on web development using Node.js', 'Learned about gulp'
         ],
         notes: ['See page Building Web Applications with Node.js & Express 4.0']
-    },
-    {
+    }, {
         key: 14,
         date: '11/14/2016',
         work: ['Finished the NPM Playbook course on Pluralsight'],
         notes: ['See Page NPM Playbook 3: Publishing your own packages']
-    },
-    {
+    }, {
         key: 15,
         date: '11/13/2016',
         work: ['Finished Codecademy course on props in React']
-    },
-    {
+    }, {
         key: 16,
         date: '11/10/2016',
-        work: ['Read Understanding module.exports and exports in Node.js',
-            'Read about React State',
-            'Learned about State Lifting',
-            'Fixed the page switching function in codegard1-react'
-        ]
-    },
-    {
+        work: ['Read Understanding module.exports and exports in Node.js', 'Read about React State', 'Learned about State Lifting', 'Fixed the page switching function in codegard1-react']
+    }, {
         key: 17,
         date: '11/7/2016',
         work: ['Continued ReactJS course on Codecademy and completed the unit on JSX'],
-        notes: ['See page: React Codecademy Course Notes',
-            'See Page: Learn ReactJS: Part 2 Notes (Codecademy)'
-        ]
-    },
-    {
+        notes: ['See page: React Codecademy Course Notes', 'See Page: Learn ReactJS: Part 2 Notes (Codecademy)']
+    }, {
         key: 18,
         date: '11/4/2016',
         work: ['Completed Learn ReactJS: Part I on CodeCademy'],
         notes: ['See page: React Codecademy Course Notes']
-    },
-    {
+    }, {
         key: 19,
         date: '10/30/2016',
-        work: ['Went to a Hacker Hours meetup at Gregory\'s coffe on 7th ave & 30th and talked to a couple of guys there, Jamie & Sam, who were studying React in a Bootcamp & who were able to give me a brief rundown of how React apps are structured, w/r/t Components, state, and props. They advised me too to look at React tutorials on Codecademy & nodeschool',
+        work: [
+            'Went to a Hacker Hours meetup at Gregory\'s coffe on 7th ave & 30th and talked t' +
+                    'o a couple of guys there, Jamie & Sam, who were studying React in a Bootcamp & w' +
+                    'ho were able to give me a brief rundown of how React apps are structured, w/r/t ' +
+                    'Components, state, and props. They advised me too to look at React tutorials on ' +
+                    'Codecademy & nodeschool',
             'Started reading the thinking-in-react guide'
         ],
         notes: ['See page: Thinking-in-react']
-    },
-    {
+    }, {
         key: 20,
         date: '10/27/2016',
-        work: ['Watched NPM course on Pluralsight',
-            'Learned a little bit about how to use Next.js',
-            'Learned more about gitignore files & how to unstage files from a commit'
+        work: [
+            'Watched NPM course on Pluralsight', 'Learned a little bit about how to use Next.js', 'Learned more about gitignore files & how to unstage files from a commit'
         ],
-        notes: ['See page: NPM Playbook part 2',
-            'See page: Next.js README.md',
-            'See page: How to unstage files in a git repository'
-        ]
-    },
-    {
+        notes: ['See page: NPM Playbook part 2', 'See page: Next.js README.md', 'See page: How to unstage files in a git repository']
+    }, {
         key: 21,
         date: '10/26/2016',
-        work: ['Watched ShareGate webinar re: the future of SharePoint',
-            'Read about next.js'
-        ]
-    },
-    {
+        work: ['Watched ShareGate webinar re: the future of SharePoint', 'Read about next.js']
+    }, {
         key: 22,
         date: '10/24/2016',
-        work: ['Completed NodeJS course on Pluralsight',
-            'Started NPM course on Pluralsight',
-            'Learned about NPM install options',
-            'Learned about gitignore files'
+        work: [
+            'Completed NodeJS course on Pluralsight', 'Started NPM course on Pluralsight', 'Learned about NPM install options', 'Learned about gitignore files'
         ],
-        notes: ['See page Introduction to NodeJS Part 8 ',
-            'See Page NPM Playbook ',
-            'See page NPM Install Dcumentation'
-        ]
-    },
-    {
+        notes: ['See page Introduction to NodeJS Part 8 ', 'See Page NPM Playbook ', 'See page NPM Install Dcumentation']
+    }, {
         date: '10/19/2016',
-        work: ['MS Ignite Deck: Build Business Apps for O365 - InfoPath, PowerApps, Flow, and More'],
+        work: ['MS Ignite Deck: Build Business Apps for O365 - InfoPath, PowerApps, Flow, and Mo' +
+                're'],
         key: 23
-    },
-    {
+    }, {
         key: 24,
         date: '10/18/2016',
-        work: ['NodeJS Pluralsight Course: Unit Testing',
-            'Read about NodeJS package.json',
-            'Learned about where NodeJS installs packages locally vs. globally '
+        work: [
+            'NodeJS Pluralsight Course: Unit Testing', 'Read about NodeJS package.json', 'Learned about where NodeJS installs packages locally vs. globally '
         ],
         notes: ['See page Introduction to NodeJS Part 7']
-    },
-    {
+    }, {
         key: 25,
         date: '10/17/2016',
-        work: ['Decks from MS Ignite 2016: ',
-            'Ultimate Field Guide to O365 Groups',
-            'SharePoint Document Libraries',
-            'Discover What\'s new and what\'s coming to SharePoint Modern Team Sites',
-            'NodeJS Pluralsight course: Interacting with the Web section'
+        work: [
+            'Decks from MS Ignite 2016: ', 'Ultimate Field Guide to O365 Groups', 'SharePoint Document Libraries', 'Discover What\'s new and what\'s coming to SharePoint Modern Team Sites', 'NodeJS Pluralsight course: Interacting with the Web section'
         ],
         notes: ['See page: Introduction to NodeJS part 6']
-    },
-    {
+    }, {
         key: 26,
         date: '10/13/2016',
         work: ['Participated in a 3hr Presentation Skills workshop hosted by HR']
-    },
-    {
+    }, {
         key: 27,
         date: '10/12/2016',
-        work: ['Participated in webinar: Is Innovative Mentoring for You?',
-            'Watched webinar: Macy\'s Save Actively Financial Wellness Series Millenials and financial matters',
-            'Watched two chapters in the NodeJS Pluralsight Course, topics were File System and  Buffers'
+        work: [
+            'Participated in webinar: Is Innovative Mentoring for You?', 'Watched webinar: Macy\'s Save Actively Financial Wellness Series Millenials and ' +
+                    'financial matters',
+            'Watched two chapters in the NodeJS Pluralsight Course, topics were File System a' +
+                    'nd  Buffers'
         ]
-    },
-    {
+    }, {
         key: 28,
         date: '10/4/2016',
-        work: ['Watched a few modules on PluralSight regarding NodeJS Events & Streams ',
-            'Events are emitted & consumed by objects',
-            'A callback is just a function that runs after a method has finished evaluating '
+        work: [
+            'Watched a few modules on PluralSight regarding NodeJS Events & Streams ', 'Events are emitted & consumed by objects', 'A callback is just a function that runs after a method has finished evaluating '
         ],
-        notes: ['See page (Pluralsight) NodeJS Streams',
-            'See page (Pluralsight) NodeJS Events'
-        ]
-    },
-    {
+        notes: ['See page (Pluralsight) NodeJS Streams', 'See page (Pluralsight) NodeJS Events']
+    }, {
         key: 29,
         date: '10/3/2016',
-        work: ['Read article on Arrow functions and did some experimenting on ES6Fiddle.net',
-            'Read article on Linting '
-        ]
-    },
-    {
+        work: ['Read article on Arrow functions and did some experimenting on ES6Fiddle.net', 'Read article on Linting ']
+    }, {
         key: 30,
         date: '9/26/2016',
-        work: ['Sat with Bobby as he explained how to use the SharePoint framework CLI tools',
-            'Learned how to scaffold from @microsoft/generator; see this page for more info',
-            'Watched some videos in the Modules, require(), and NPM chapter of the NodeJS Pluralsight course'
+        work: [
+            'Sat with Bobby as he explained how to use the SharePoint framework CLI tools', 'Learned how to scaffold from @microsoft/generator; see this page for more info', 'Watched some videos in the Modules, require(), and NPM chapter of the NodeJS Plu' +
+                    'ralsight course'
         ]
-    },
-    {
+    }, {
         key: 31,
         date: '9/22/2016',
         work: ['PnP video re: SharePoint Framework', 'Learned about npm']
-    },
-    {
+    }, {
         key: 32,
         date: '9/21/2016',
         work: ['Read Grokking Scope in JavaScript']
