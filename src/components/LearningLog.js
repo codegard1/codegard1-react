37ff37fdae9a48321a8639cbdcfeae2d6c655213
Chris Odegard <c.odegard@gmail.com>
2018-03-13 22:20:59 -0400

    updated render of list cell

--- src/components/LearningLog.js
+++ src/components/LearningLog.js
@@ -1,13 +1,9 @@
 import React from "react";
-import {
-  FocusZone,
-  FocusZoneDirection
-} from "office-ui-fabric-react/lib/FocusZone";
 import { TextField } from "office-ui-fabric-react/lib/TextField";
 import { List } from "office-ui-fabric-react/lib/List";
 
 /* custom stuff */
-import  BaseComponent  from "./BaseComponent";
+import BaseComponent from "./BaseComponent";
 import { learningLog2016 } from "./learningLog2016";
 import "./learninglog.css";
 
@@ -72,30 +68,27 @@ export class LearningLog extends BaseComponent {
 
   _onRenderCell(item, index) {
     return (
-      <div className="itemCell" data-is-focusable={true}>
-
-        <div className="itemContent">
-          <div>
+      <div className="ms-Grid itemCell" data-is-focusable={true}>
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm3">
             <span className="itemName ms-font-xl">{item.date}</span>
             <span className="itemIndex ms-font-xs">{`#${index}`}</span>
           </div>
-          <div className="itemDesc ms-font-s">
 
-            <p>
+          <div className="ms-Grid-col ms-sm5">
+            <p className="ms-font-s">
               <i className="ms-Icon ms-Icon--QuickNote" /> &nbsp; Work
             </p>
-
-            <ul>
+            <ul className="ms-font-s">
               {item.work.length > 0 ? item.work : "None"}
             </ul>
+          </div>
 
+          <div className="ms-Grid-col ms-sm4">
             <p className="ms-font-s">
               <i className="ms-Icon ms-Icon--QuickNote" /> &nbsp; Notes
             </p>
-
-            <ul>
-              {item.notes.length > 0 ? item.notes : "None"}
-            </ul>
+            <ul>{item.notes.length > 0 ? item.notes : "None"}</ul>
           </div>
         </div>
       </div>
@@ -103,28 +96,39 @@ export class LearningLog extends BaseComponent {
   }
 
   render() {
+    /* List filtering */
     let originalItems = this.itemsArray;
     let items = this.state.items;
-    let resultCountText = items.length === originalItems.length
-      ? ""
-      : ` (${items.length} of ${originalItems.length} shown)`;
+    let resultCountText =
+      items.length === originalItems.length
+        ? ""
+        : ` (${items.length} of ${originalItems.length} shown)`;
 
     return (
       <div id="learning-log">
         <p className="ms-font-su">Learning Log</p>
-        <FocusZone direction={FocusZoneDirection.vertical}>
-          <TextField
-            label={`Filter by Date` + resultCountText}
-            onBeforeChange={this._onFilterChanged}
-          />
-          <List
-            items={items}
-            onRenderCell={this._onRenderCell}
-            startIndex={0}
-            renderedWindowsAhead={1}
-            renderedWindowsBehind={1}
-          />
-        </FocusZone>
+        <p>
+          This is a list of things I've read and done that are code-related.
+        </p>
+
+        <div className="ms-Grid">
+          <div className="ms-Grid-row">
+            <div className="ms-Grid-col ms-sm3">
+              <TextField
+                label={`Filter by Date` + resultCountText}
+                onBeforeChange={this._onFilterChanged}
+              />
+            </div>
+          </div>
+        </div>
+
+        <List
+          items={items}
+          onRenderCell={this._onRenderCell}
+          startIndex={0}
+          renderedWindowsAhead={1}
+          renderedWindowsBehind={1}
+        />
       </div>
     );
   }
