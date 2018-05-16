981ff51e268aba3775afef4d996e282f126b6945
Chris Odegard <c.odegard@gmail.com>
2018-05-15 21:00:47 -0400

    removed redundant Fabric import

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import * as T from "prop-types";
 import { Route } from "react-router-dom";
-import { Fabric } from "office-ui-fabric-react/lib/Fabric";
 
 /* custom stuff */
 import { NavDefinition } from "./pageContent";
 import "./Page.css";
 
-/* Wrapper for page content that passes props from Page to any children components */
-const BasePage = props => <Fabric id="BasePage">{props.children}</Fabric>;
-
-/* create array of routes that return page components */
+/* create array of routes that return page content components */
 const routes = NavDefinition[0].links.map(item => (
   <Route
     key={`route-${item.key}`}
