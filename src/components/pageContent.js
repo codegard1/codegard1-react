512cc1d135dd27e44e0f5677927c6ed5f742d7da
Chris Odegard <c.odegard@gmail.com>
2020-09-28 22:54:54 -0400

    Set blackjack Table as the only option in navDefinition

--- src/components/pageContent.js
+++ src/components/pageContent.js
@@ -3,25 +3,9 @@ import React from "react";
 /* custom stuff */
 import Table from "./blackjack/Table";
 
-// Home page component
-const home = (
-  <div id="Home">
-    <h1 className="ms-font-su">Hello, world!</h1>
-  </div>
-);
-
 export const NavDefinition = [
   {
     links: [
-      {
-        category: "Pages",
-        iconProps: {
-          iconName: "Home"
-        },
-        key: "home",
-        name: "Home",
-        content: home
-      },
       {
         category: "Pages",
         iconProps: {
